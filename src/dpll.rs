06944eee6ffe6e6430117ba914008743fd41bd35
arata <49086220+arata-nvm@users.noreply.github.com>
2021-10-22 13:13:45 +0000

    feat: implement DPLL

--- src/dpll.rs
+++ src/dpll.rs
@@ -1,6 +1,37 @@
-use crate::types::Cnf;
+use crate::types::*;
 
-pub fn solve(cnf: Cnf) -> bool {
+pub fn solve(mut cnf: Cnf) -> bool {
+    // unit rule
+    for literal in cnf.find_unit_clauses() {
+        cnf.remove_clauses_which_has(&literal);
+        cnf.remove_from_all(&literal.inverted());
+    }
+
+    if cnf.is_consistent() {
+        return true;
+    }
+    if cnf.has_empty_clause() {
+        return false;
+    }
+
+    // splitting rule
+    let literal = match cnf.head_literal() {
+        Some(l) => l,
+        None => return false,
+    };
+
+    let mut cnf_true = cnf.clone();
+    cnf_true.assume(literal.clone());
+    if solve(cnf_true) {
+        return true;
+    }
+
+    let mut cnf_false = cnf.clone();
+    cnf_false.assume(literal.inverted());
+    if solve(cnf_false) {
+        return true;
+    }
+
     false
 }
 
@@ -27,5 +58,34 @@ mod tests {
             Clause::new(vec![Literal::new("-a")]),
         ]);
         assert_eq!(super::solve(cnf), false);
+    }
+
+    #[test]
+    fn dpll_4() {
+        let cnf = Cnf::new(vec![Clause::new(vec![
+            Literal::new("a"),
+            Literal::new("b"),
+        ])]);
+        assert_eq!(super::solve(cnf), true);
+    }
+
+    #[test]
+    fn dpll_5() {
+        let cnf = Cnf::new(vec![
+            Clause::new(vec![Literal::new("a"), Literal::new("-b")]),
+            Clause::new(vec![Literal::new("-a"), Literal::new("b")]),
+        ]);
+        assert_eq!(super::solve(cnf), true);
+    }
+
+    #[test]
+    fn dpll_6() {
+        let cnf = Cnf::new(vec![
+            Clause::new(vec![Literal::new("x1"), Literal::new("x2")]),
+            Clause::new(vec![Literal::new("-x1"), Literal::new("x3")]),
+            Clause::new(vec![Literal::new("x1"), Literal::new("-x3")]),
+            Clause::new(vec![Literal::new("-x2"), Literal::new("-x3")]),
+        ]);
+        assert_eq!(super::solve(cnf), true);
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,14 +1,14 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Cnf {
     pub clauses: Vec<Clause>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Clause {
     pub literals: Vec<Literal>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Literal {
     pub name: String,
     pub inverted: bool,
@@ -18,11 +18,57 @@ impl Cnf {
     pub fn new(clauses: Vec<Clause>) -> Self {
         Self { clauses }
     }
+
+    pub fn is_consistent(&self) -> bool {
+        self.clauses.is_empty()
+    }
+
+    pub fn has_empty_clause(&self) -> bool {
+        self.clauses.iter().any(|c| c.is_empty())
+    }
+
+    pub fn find_unit_clauses(&self) -> Vec<Literal> {
+        self.clauses
+            .iter()
+            .filter(|c| c.literals.len() == 1)
+            .map(|c| c.literals[0].clone())
+            .collect()
+    }
+
+    pub fn head_literal(&self) -> Option<Literal> {
+        self.clauses.get(0).and_then(|c| c.literals.get(0)).cloned()
+    }
+
+    pub fn remove_clauses_which_has(&mut self, literal: &Literal) {
+        self.clauses.retain(|c| !c.has_literal(literal));
+    }
+
+    pub fn remove_from_all(&mut self, literal: &Literal) {
+        for i in 0..self.clauses.len() {
+            self.clauses[i].remove(literal);
+        }
+    }
+
+    pub fn assume(&mut self, literal: Literal) {
+        self.clauses.push(Clause::new(vec![literal]));
+    }
 }
 
 impl Clause {
     pub fn new(literals: Vec<Literal>) -> Self {
         Self { literals }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.literals.is_empty()
+    }
+
+    pub fn has_literal(&self, literal: &Literal) -> bool {
+        self.literals.iter().any(|l| l == literal)
+    }
+
+    pub fn remove(&mut self, literal: &Literal) {
+        self.literals.retain(|l| l != literal);
     }
 }
 
@@ -38,6 +84,13 @@ impl Literal {
                 name,
                 inverted: false,
             },
+        }
+    }
+
+    pub fn inverted(&self) -> Self {
+        Self {
+            name: self.name.clone(),
+            inverted: !self.inverted,
         }
     }
 }
