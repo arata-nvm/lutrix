033cec471af85ed6201c710ccfeb00d176ad41df
arata <49086220+arata-nvm@users.noreply.github.com>
2021-10-22 16:21:07 +0000

    feat: implement CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+
+use lutrix::{dimacs, dpll, types::Variable};
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        println!("usage: lutrix <input-file>");
+        return;
+    }
+
+    println!("[*] reading file: {}", args[1]);
+    let input = std::fs::read_to_string(&args[1]).expect("cannot read file");
+    let formula = dimacs::parse(&input);
+    println!("[*] formula = {}", formula);
+    match dpll::solve(formula) {
+        dpll::SatResult::Sat(solution) => {
+            println!("SAT");
+
+            let mut vars = solution.keys().collect::<Vec<&Variable>>();
+            vars.sort();
+            for var in vars {
+                println!("x{} = {}", var, solution[var]);
+            }
+        }
+        dpll::SatResult::Unsat => {
+            println!("UNSAT");
+        }
+    }
 }
