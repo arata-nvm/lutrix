ee99efc351c094e0a88e5c81f3740436c367f941
arata <49086220+arata-nvm@users.noreply.github.com>
2021-11-01 06:54:54 +0000

    test: add tests for bit_blaster

---
+++ tests/bit_blaster.rs
@@ -0,0 +1,25 @@
+use lutrix::{bit_blaster, dpll, smtlib};
+
+#[test]
+fn bb_1() {
+    let input = "(declare-fun x () Bool)
+                (declare-fun y () Bool)
+                (assert (not (= (not (or x y)) (and (not x) (not y)))))";
+    let problem = smtlib::parse(input);
+    let (formula, _) = bit_blaster::transform(problem);
+    let result = dpll::solve(formula);
+    assert!(result.is_unsat());
+}
+
+#[test]
+fn bb_2() {
+    let input = "(declare-fun x () (_ BitVec 8))
+                (declare-fun y () (_ BitVec 8))
+                (assert (= x #x05))
+                (assert (= y #x03))
+                (assert (= (bvadd x y) #x08))";
+    let problem = smtlib::parse(input);
+    let (formula, _) = bit_blaster::transform(problem);
+    let result = dpll::solve(formula);
+    assert!(result.is_sat());
+}
