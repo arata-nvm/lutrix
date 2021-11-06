9a7081905e949f406934e585392286a2896ed351
arata <49086220+arata-nvm@users.noreply.github.com>
2021-11-06 04:43:03 +0000

    test: add tests for smt solver

--- tests/smt.rs
+++ tests/smt.rs
@@ -2,18 +2,144 @@ use lutrix::smt::Solver;
 use lutrix::{int, op};
 
 #[test]
-fn solver_1() {
+fn bvnot() {
     let mut s = Solver::new();
-    let a = s.new_variable("a", 8);
-    let b = s.new_variable("b", 8);
-    let c = s.new_variable("c", 8);
-    s.assert(op!(a == int!(30, 8)));
-    s.assert(op!(b == int!(8, 8)));
-    s.assert(op!(c == op!(a + b)));
+    let a = s.new_variable("x1", 2);
+    let b = s.new_variable("x2", 2);
+    s.assert(op!(a == int!(0b01, 2)));
+    s.assert(op!(a == op!(!b)));
+    s.assert(op!(b == int!(0b10, 2)));
     assert!(s.check());
+}
 
-    let model = s.model();
-    assert_eq!(model["a"], 30);
-    assert_eq!(model["b"], 8);
-    assert_eq!(model["c"], 38);
+#[test]
+fn bvand() {
+    let mut s = Solver::new();
+    let a = s.new_variable("x1", 4);
+    let b = s.new_variable("x2", 4);
+    let c = s.new_variable("x3", 4);
+    s.assert(op!(a == int!(0b0011, 4)));
+    s.assert(op!(b == int!(0b0101, 4)));
+    s.assert(op!(c == op!(a & b)));
+    s.assert(op!(c == int!(0b0001, 4)));
+    assert!(s.check());
+}
+
+#[test]
+fn bvor() {
+    let mut s = Solver::new();
+    let a = s.new_variable("x1", 4);
+    let b = s.new_variable("x2", 4);
+    let c = s.new_variable("x3", 4);
+    s.assert(op!(a == int!(0b0011, 4)));
+    s.assert(op!(b == int!(0b0101, 4)));
+    s.assert(op!(c == op!(a | b)));
+    s.assert(op!(c == int!(0b0111, 4)));
+    assert!(s.check());
+}
+
+#[test]
+fn bvxor() {
+    let mut s = Solver::new();
+    let a = s.new_variable("x1", 4);
+    let b = s.new_variable("x2", 4);
+    let c = s.new_variable("x3", 4);
+    s.assert(op!(a == int!(0b0011, 4)));
+    s.assert(op!(b == int!(0b0101, 4)));
+    s.assert(op!(c == op!(a ^ b)));
+    s.assert(op!(c == int!(0b0110, 4)));
+    assert!(s.check());
+}
+
+#[test]
+fn bvadd() {
+    let mut s = Solver::new();
+    let a = s.new_variable("x1", 4);
+    let b = s.new_variable("x2", 4);
+    let c = s.new_variable("x3", 4);
+    s.assert(op!(a == int!(0b0111, 4)));
+    s.assert(op!(b == int!(0b0001, 4)));
+    s.assert(op!(c == op!(a + b)));
+    s.assert(op!(c == int!(0b1000, 4)));
+    assert!(s.check());
+}
+
+#[test]
+fn bvsub() {
+    let mut s = Solver::new();
+    let a = s.new_variable("x1", 4);
+    let b = s.new_variable("x2", 4);
+    let c = s.new_variable("x3", 4);
+    s.assert(op!(a == int!(0b0100, 4)));
+    s.assert(op!(b == int!(0b0001, 4)));
+    s.assert(op!(c == op!(a - b)));
+    s.assert(op!(c == int!(0b0011, 4)));
+    assert!(s.check());
+}
+#[test]
+fn bvmul() {
+    let mut s = Solver::new();
+    let a = s.new_variable("x1", 4);
+    let b = s.new_variable("x2", 4);
+    let c = s.new_variable("x3", 4);
+    s.assert(op!(a == int!(0b0011, 4)));
+    s.assert(op!(b == int!(0b0100, 4)));
+    s.assert(op!(c == op!(a * b)));
+    s.assert(op!(c == int!(0b1100, 4)));
+    assert!(s.check());
+}
+
+#[test]
+fn bvshl() {
+    let mut s = Solver::new();
+    let a = s.new_variable("x1", 4);
+    let b = s.new_variable("x2", 4);
+    s.assert(op!(a == int!(0b0011, 4)));
+    s.assert(op!(b == op!(a << 3)));
+    s.assert(op!(b == int!(0b1000, 4)));
+    assert!(s.check());
+}
+#[test]
+fn bvshr() {
+    let mut s = Solver::new();
+    let a = s.new_variable("x1", 4);
+    let b = s.new_variable("x2", 4);
+    s.assert(op!(a == int!(0b0011, 4)));
+    s.assert(op!(b == op!(a >> 1)));
+    s.assert(op!(b == int!(0b0001, 4)));
+    assert!(s.check());
+}
+
+#[test]
+fn bvult() {
+    let mut s = Solver::new();
+    let c0 = int!(0, 1);
+    let c1 = int!(1, 1);
+    s.assert(op!(c0 < c1));
+    assert!(s.check());
+}
+
+#[test]
+fn bvule() {
+    let mut s = Solver::new();
+    let c1 = int!(1, 1);
+    s.assert(op!(c1 <= c1));
+    assert!(s.check());
+}
+
+#[test]
+fn bvugt() {
+    let mut s = Solver::new();
+    let c0 = int!(0, 1);
+    let c1 = int!(1, 1);
+    s.assert(op!(c1 > c0));
+    assert!(s.check());
+}
+
+#[test]
+fn bvuge() {
+    let mut s = Solver::new();
+    let c1 = int!(1, 1);
+    s.assert(op!(c1 >= c1));
+    assert!(s.check());
 }
